f86b491bd89189441f8fdc9e990b169aa2a8dea4
Franco <fran_kol@hotmail.com>
2022-03-09 11:19:28 -0300

    Desafío 3 y 4 - Menú e-commerce y Componentes 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./container/ItemListContainer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer titulo="Hola Mundo Coders" />
+    </>
+  );
+}
+
+export default App;
