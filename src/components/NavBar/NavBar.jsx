d2e2eb7e28c0e7ea14a2059a6c1d5e4d3364b658
Franco <fran_kol@hotmail.com>
2022-03-15 11:50:18 -0300

    Desafío 5 y 6 - Contador con botón + Catálogo con MAPS y Promises

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,42 +1,88 @@
 import React from "react";
-import CartWidget from "../CartWidget";
+import CartWidget from "./CartWidget";
 import "../NavBar/navBar.css";
 import Logo from "./images/logogringa.png";
 
 function NavBar() {
   return (
     <header>
-      <nav>
-        <div className="headerNavAncho">
-          <div className="logo">
-            <a href=" ">
-              <img src={Logo} alt="logo" className="logo__img--center" />
-            </a>
-          </div>
+      <nav className="navbar navbar-expand-sm">
+        <div className="container">
+          {/* <!-- Logo --> */}
+          <a className="navbar-brand ms-3" href="index.html">
+            <img
+              src={Logo}
+              alt="logo"
+              className="logo__img--center wow animate__animated animate__flipInX"
+            />
+          </a>
 
-          <div className="menu">
-            <ul className="menu__items">
-              <li>
-                <a href=" ">HOME</a>
+          {/* <!-- Boton Hamburguesa --> */}
+          <button
+            className="navbar-toggler custom-toggler me-4 wow animate__animated animate__bounceInDown"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* <!-- Menu listado de opciones --> */}
+          <div className="collapse navbar-collapse pe-5" id="navbarNav">
+            <ul id="menuHeader" className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a
+                  className="nav-link fw-bold text-uppercase menuSecciones wow animate__animated animate__bounceInDown"
+                  href="index.html"
+                  data-wow-delay="0.2s"
+                >
+                  Home
+                </a>
               </li>
-              <li>
-                <a href=" ">NOSOTROS</a>
+              <li className="nav-item">
+                <a
+                  className="nav-link  fw-bold text-uppercase menuSecciones wow animate__animated animate__bounceInDown"
+                  href="html/nosotros.html"
+                  data-wow-delay="0.3s"
+                >
+                  Nosotros
+                </a>
               </li>
-              <li>
-                <a href=" ">SERVICIOS</a>
+              <li className="nav-item">
+                <a
+                  className="nav-link  fw-bold text-uppercase menuSecciones wow animate__animated animate__bounceInDown"
+                  href="html/servicios.html"
+                  data-wow-delay="0.4s"
+                >
+                  Servicios
+                </a>
               </li>
-              <li>
-                <a href=" ">GALERIA</a>
+              <li className="nav-item">
+                <a
+                  className="nav-link  fw-bold text-uppercase menuSecciones wow animate__animated animate__bounceInDown"
+                  href="html/galeria.html"
+                  data-wow-delay="0.5s"
+                >
+                  Galería
+                </a>
               </li>
-              <li>
-                <a href=" ">CONTACTO</a>
-              </li>
-              <li>
-                <CartWidget />
+              <li className="nav-item">
+                <a
+                  className="nav-link  fw-bold text-uppercase menuSecciones wow animate__animated animate__bounceInDown"
+                  href="html/contacto.html"
+                  data-wow-delay="0.6s"
+                >
+                  Contacto
+                </a>
               </li>
             </ul>
           </div>
-        </div>
+          <CartWidget />
+        </div>{" "}
+        {/*<!-- container -->*/}
       </nav>
     </header>
   );
